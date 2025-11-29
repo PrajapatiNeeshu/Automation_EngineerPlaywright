04aa3a1056105053babe1ffbd7e5aa9df447511a
NeeshuBiet <neeshubiet@gmail.com>
2025-11-29 21:33:21 +0530

    delete the extra file

---
+++ tests/Rahul16client.spec.js
@@ -0,0 +1,17 @@
+const { test, expect } = require("@playwright/test");
+test("@Web Client App login", async ({ page }) => {
+  //js file- Login js, DashboardPage
+  const email = "eddygpm@gmail.com";
+  const productName = "zara coat 3";
+  const products = page.locator(".card-body");
+  await page.goto("https://rahulshettyacademy.com/client");
+  await page.locator("#userEmail").fill(email);
+  await page.locator("#userPassword").fill("Bpost@9012_");
+  await page.locator("[value='Login']").click();
+  // await page.waitForLoadState("networkidle"); //asking is when network is loading at this points
+
+  await page.locator(".card-body b").first().waitFor(); //.card-body b   .card-body b have parent to child tranvers so it will be print parent to child
+  const titles = await page.locator(".card-body b").allTextContents(); // print all test contents at the some dom in html DOM.
+
+  console.log(titles);
+});
