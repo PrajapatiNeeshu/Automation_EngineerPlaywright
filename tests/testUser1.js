8d09fd521b5689f117f11ba2d3c97e37e866fef2
PrajapatiNeeshu <neeshubiet@gmail.com>
2025-12-23 20:01:56 +0530

    added new file

--- tests/testUser1.js
+++ tests/testUser1.js
@@ -1,3 +1,28 @@
-// dummy await expect(page).toHaveTitle(``);
 
-// dummy await page.goto('http://localhost:3000/career');
+//testUser2();
+
+export class testUser4 {
+    constructor( a = null ) { 
+
+    }
+      testUser3() { 
+    let a = 10;     
+    let b = 20;
+    let sum = a + b;    
+
+console.log("This is testUser3function");
+}
+
+
+   testUser2() { 
+    let a = 10;
+    let b = 20;
+    let sum = a + b;    
+
+console.log("This is testUser2 function");
+   }
+}
+
+let user = new testUser4();
+user.testUser2()
+user.testUser3()
